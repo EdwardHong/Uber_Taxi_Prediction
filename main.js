f4259f6766a7b62732b102577328200a33ff1974
pratheeksh <pks329@nyu.edu>
2016-04-18 09:02:11 -0400

    working main.js

--- main.js
+++ main.js
@@ -1,7 +1,59 @@
 $(document).ready(function() {
 
-var xKey = 'displacement', yKey = 'mpg';
-var xValue = function(d) { return d[xKey]; }, yValue = function(d) { return d[yKey]; };
+d3.csv("car.csv", function(d) {
+ return {
+
+  };
+}, 
+function(dataset) {
+    d = dataset;
+    data  = dataset;
+    values = dataset;
+    var select_x = document.getElementById("sel-x");
+    var options = Object.keys(values[0])
+    for(var i = 0; i < options.length-1; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select_x.appendChild(el);
+    }
+    
+    var select_y = document.getElementById("sel-y");
+
+    for(var i = 0; i < options.length-1; i++) {
+        var opt = options[i];
+        var el = document.createElement("option");
+        el.textContent = opt;
+        el.value = opt;
+        select_y.appendChild(el);
+    }
+   
+});
+    var sel_x = document.getElementById("sel-x");
+    var selected_x = sel_x.options[sel_x.selectedIndex].value;
+    var sel_y = document.getElementById("sel-x");
+    var selected_y = sel_y.options[sel_y.selectedIndex].value;
+
+    $("#sel-x").change(function() {
+        var selected_x = $("#sel-x option:selected").val();
+        draw(selected_x,selected_y);
+    });
+    $("#sel-y").change(function() {
+        var selected_y = $("#sel-y option:selected").val();
+        draw(selected_x,selected_y);
+    });
+
+    console.log(selected_x)
+
+});
+
+var draw = function(xKey,yKey) {
+
+//var xKey = 'displacement';
+//var yKey = 'mpg';
+var xValue = function(d) { return d[xKey]; };
+var yValue = function(d) { return d[yKey]; };
 
 var margin = {top: 30, right: 20, bottom: 30, left: 50},
     width = 400 - margin.left - margin.right,
@@ -31,21 +83,21 @@ var svg = d3.select("body")
               "translate(" + margin.left + "," + margin.top + ")");
 // Get the data
 d3.csv("car.csv", function(error, data) {
-	console.log(data[0])
+    console.log(data[0])
    data.forEach(function(d) {
-	    // csv data are input one row at a time
-		var key = Object.keys(d)
-		for(var i = 0; i < key.length-1; i++) {
-		// change string into number format
-			if (!isNaN(d[key[i]])){
-				d[key[i]] = +d[key[i]];
-				}
-		}
-	    
-		d[xKey] = +d[xKey];
+        // csv data are input one row at a time
+        var key = Object.keys(d)
+        for(var i = 0; i < key.length-1; i++) {
+        // change string into number format
+            if (!isNaN(d[key[i]])){
+                d[key[i]] = +d[key[i]];
+                }
+        }
+        
+        d[xKey] = +d[xKey];
         d[yKey] = +d[yKey];
     });
-	console.log(data[0])
+    console.log(data[0])
     // Scale the range of the data
     x.domain([d3.min(data, xValue)-10, d3.max(data, xValue)+30]);
     y.domain([d3.min(data, yValue)-5, d3.max(data, yValue)+5]);
@@ -54,76 +106,41 @@ d3.csv("car.csv", function(error, data) {
         .attr("class", "line")
         .data(data);
     // Add the scatterplot
-    svg.selectAll("dot")
-        .data(data)
-      .enter().append("circle")
+    var dots = svg.selectAll("dot").data(data);
+    dots.attr("class","update");
+
+    //UPDATE
+    dots.enter().append('circle')
+        .attr("class", "enter")
         .attr("r", 3.5)
         .attr("cx", function(d) { return x(d[xKey]); })
-        .attr("cy", function(d) { return y(d[yKey]); })
-		
+        .attr("cy", function(d) { return y(d[yKey]); });
+    dots.exit().remove();
+
     // Add the X Axis
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis)
-	  .append("text")
+      .append("text")
         .attr("class", "label")
         .attr("x", width)
         .attr("y", -6)
         .style("text-anchor", "end")
         .text(xKey);
-	  
+      
     // Add the Y Axis
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis)
-	  .append("text") 
+      .append("text") 
         .attr("class", "label")
         .attr("x", 30)
         .attr("y", -6)
-		.attr("transform", "rotate(90)")
+        .attr("transform", "rotate(90)")
         .style("text-anchor", "end")
         .text(yKey);
 });
 
 
-
-d3.csv("car.csv", function(d) {
- return {
-    mpg : d.mpg,
-    cylinders : d.cylinders,
-    displacement : d.displacement,
-    horsepower : d.horsepower,
-    weight : d.weight,
-    acceleration : d.acceleration,
-    model_year : d["model.year"],
-    origin : d.origin
-  };
-}, function(dataset) {
-	d = dataset;
-	data  = dataset;
-  	var select = document.getElementById("sel-x");
-	var options = Object.keys(dataset[0])
-	for(var i = 0; i < options.length-1; i++) {
-    var opt = options[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
-    }
-    
-    var select2 = document.getElementById("sel-y");
-	//var options = ["mpg",	"cylinders",	"displacement",	"horsepower",	"weight",	"acceleration",	"model.year"];
-	for(var i = 0; i < options.length-1; i++) {
-    var opt = options[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select2.appendChild(el);
-	}
-	
-	
-
-});
-});
-
+};
