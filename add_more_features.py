885eaffe28ed4b2f9e78eff69e43ea10c8868508
swh274@nyu.edu <swh274@nyu.edu>
2016-05-12 17:34:01 -0400

    adding features

--- add_more_features.py
+++ add_more_features.py
@@ -20,9 +20,14 @@ def add_more_features(attribute_list):
 		snow_flag = 1 	
 
 	for i in range(0, len(attribute_list)-2):
-		enhanced_list[i] = attribute_list[i]
+		enhanced_list.append(attribute_list[i])
 
 	enhanced_list.append(temp_avg)
 	enhanced_list.append(rain_flag)
 	enhanced_list.append(snow_flag)
+	enhanced_list.append(num_uber)
+	enhanced_list.append(num_taxi)
 	return enhanced_list
+
+new_list = add_more_features([20150630.0, 0.04, 0.0, 82.0, 68.0, 0.0, 4.92, 170.0, 170.0, 14.1, 25.1, 1.0, 22.0, 5749.0, 22815.0])
+print new_list
