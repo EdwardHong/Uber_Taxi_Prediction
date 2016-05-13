2105321c184d7d135c52efb5a2453acf355f104f
pratheeksh <pks329@nyu.edu>
2016-05-12 20:56:20 -0400

    functions for producing different test & train data

--- classifier.py
+++ classifier.py
@@ -3,19 +3,41 @@ import numpy as np
 from sklearn import datasets
 from sklearn import ensemble
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import precision_recall_fscore_support
-
+import add_more_features
 ############################import###################################################
 # Load data
+def getData(data,type):
+    test_data = []
+    test_target = []
+    train_data = []
+    train_target = []
+    if type == 1:
+        featnames = np.array(['DATE', 'PRCP', 'SNWD', 'TMAX', 'TMIN', 'SNOW',
+                         'AWND', 'WDF2', 'WDF5', 'WSF2', 'WSF5', 'WT01', 'HOUR'])
+
+        for i, row in enumerate(data):
+            features = row[:-2]
+            target = row.pop(-1)
+            if i % 10 == 0:
+
+                test_data.append(features)
+                test_target.append(target)
+            else:
+
+                train_data.append(features)
+                train_target.append(target)
+
+    return featnames, np.array(train_data), np.array(test_data), \
+               np.array(train_target),np.array(test_target)
+
+
 data = open("data/final_data", "rb")
+datalist = []
+for i, row in enumerate(data):
 
-boston = datasets.load_boston()
-
-test_data = []
-train_data = []
-test_target = []
-target = []
-for i ,row in enumerate(data):
     values = row.strip('\n').split('\t')
     date = values[0]
     date = float(date)
@@ -23,39 +45,32 @@ for i ,row in enumerate(data):
     new_vals = []
     for v in vals:
         new_vals.append(float(v))
-    tup = (new_vals[:-1], new_vals.pop(-1))
-    features = [date] + tup[0] + [tup[1]]
-    if i % 10 == 0:
-        test_data.append(features)
-        test_target.append(round(tup[1]/100))
-    else:
 
-        train_data.append(features)
-        target.append(round(tup[1]/100))
-
+    datalist.append([date]+new_vals)
 
 '''X, y = datasets.make_classification(n_samples=100000, n_features=20,
                                     n_informative=2, n_redundant=2)
 
 train_samples = 100  # Samples used for training the models
 '''
-X_train = np.array(train_data)
-X_test = np.array(test_data)
-y_train = np.array(target)
-y_test = np.array(test_target)
-
+feature_names, X_train, X_test, y_train, y_test = getData(datalist,1)
 
 
 ###############################################################################
 # Fit regression model
-params = {'n_estimators': 1500, 'max_depth': 2, 'min_samples_split': 1,
-          'learning_rate': 0.1, 'loss': 'ls'}
+params = {'n_estimators': 5000, 'max_depth': 2, 'min_samples_split': 1,
+          'learning_rate': 0.2, 'loss': 'ls' }
 clf = ensemble.GradientBoostingRegressor(**params)
 
 clf.fit(X_train, y_train)
+print clf.feature_importances_
+mse_train = mean_squared_error(y_train, clf.predict(X_train))
+print("MSE: %.4f" % mse_train)
+
 mse = mean_squared_error(y_test, clf.predict(X_test))
 print("MSE: %.4f" % mse)
-
+absError = mean_absolute_error(y_test, clf.predict(X_test))
+print absError
 ###############################################################################
 # Plot training deviance
 
@@ -71,7 +86,8 @@ for i, y_pred in enumerate(clf.staged_predict(X_test)):
         results.append(round(val))
 
     #print y_test,np.array(results)
-    print precision_recall_fscore_support(y_test, np.array(results),average='macro'),mse
+#print precision_recall_fscore_support(y_test, np.array(results),average='macro'),mse
+print explained_variance_score(y_test,np.array(results),multioutput='uniform_average')
 plt.figure(figsize=(12, 6))
 plt.subplot(1, 2, 1)
 plt.title('Deviance')
@@ -92,7 +108,7 @@ sorted_idx = np.argsort(feature_importance)
 pos = np.arange(sorted_idx.shape[0]) + .5
 plt.subplot(1, 2, 2)
 plt.barh(pos, feature_importance[sorted_idx], align='center')
-# plt.yticks(pos, boston.feature_names[sorted_idx])
+plt.yticks(pos, feature_names[sorted_idx])
 plt.xlabel('Relative Importance')
 plt.title('Variable Importance')
 plt.show()
